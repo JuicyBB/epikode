54ea4f316e7faa844d61cbed0d29de8e6d27821c
Joyce <JuicyBB@users.noreply.github.com>
2017-09-28 15:48:12 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,24 @@ $(window).load(function() {
 });
 
 $(document).ready(function(){
+
+  $(".bg").interactive_bg();
+  $(window).resize(function() {
+      $(".bg > .ibg-bg").css({
+        width: $(window).outerWidth(),
+        height: $(window).outerHeight()
+      })
+   })
+
+   var typed = new Typed('.typed', {
+     strings: ["Websites", "Ecommerce Platforms", "Web Apps", "Mobile Apps", "Marketing Solutions"],
+     typeSpeed: 40,
+     backSpeed: 20,
+     loop: true,
+     cursorChar: '_',
+     loopCount: Infinity
+   });
+
   $("#counter").countdown({
   until: new Date(2017, 11, 01),
   format: 'dHMS'
@@ -13,6 +31,5 @@ $(document).ready(function(){
   minFontSize: '20px',
   maxFontSize: '50px'
   });
-});
 
-$(".bg").interactive_bg();
+});
